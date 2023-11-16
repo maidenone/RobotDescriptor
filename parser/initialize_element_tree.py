7c4aa12796af7f5d875f5f98fe3071ce99f5227e
Joseph Mbayi <josephmbayi75@gmail.com>
2023-11-16 22:48:15 +0300

    completed the parser and element tree generation

--- parser/initialize_element_tree.py
+++ parser/initialize_element_tree.py
@@ -1,19 +1,8 @@
-import   RC_parse_sdf
+from . import  RC_parse_sdf
 import xml.etree.ElementTree as ET
 
 INCLUDE_COMMENTS=False
-# #to be used later 
  
-#         #set parent node
-#         if Element ==self.root:
-#             #root element will have a none parent 
-#             ElemDict["parent"]=None
-#         else:
-#             # the parent is most  probably be the previously added element 
-#             ElemDict["parent"]=self.parent_stack[-2]
-
-# #end
-#convert dictionary to and element tree  that will be filled with data 
 class convdict_2_tree:
     def __init__(self):
         #initialize class
@@ -23,12 +12,11 @@ class convdict_2_tree:
         #a stack of parent elements
         #this stack is provided to allow for  having a parent  key in the dictionary 
         #to help when creating an xnl tree by adding subnode 
-        self.parent_stack=[]
-        
+       
         #get stuff started 
         self.create_root()
         self.construct_tree(self._root_elem,self.structured["children"])
-        
+        self.e_tree=ET.ElementTree(self._root_elem)
     #create the root element 
     #this does not need other properties as it does not have them ,this I'm sure of 
     #so no need to add them
@@ -39,11 +27,8 @@ class convdict_2_tree:
                 self._root_elem.set(attr.name,attr.default)
         
         
-    
     def construct_tree(self,parent_elem,st_lst):
         
-        #add the root element to the list of parents 
-        self.parent_stack.append(parent_elem)
         attr=dict()
         for child in st_lst:
             if child["attributes"] !=None:
@@ -54,12 +39,11 @@ class convdict_2_tree:
                 s.text=child["default"]
             if len(child["children"]) >0:
                 self.construct_tree(s,child["children"])
-        
-    def add_comment(self,tree_elem,comment:str):
-        if INCLUDE_COMMENTS:
-            self._root_elem.append(comment)
-        else:
-            pass
+    @property
+    def tree(self):
+        return self.e_tree
     
 if __name__=="__main__":
     t=convdict_2_tree()
+    tree=t.tree
+    tree.write("out.xml",encoding='utf8',xml_declaration=True)
