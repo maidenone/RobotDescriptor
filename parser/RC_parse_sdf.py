dc5595ffed4cd35e878e5450af5620a779da3141
Joseph Mbayi <josephmbayi75@gmail.com>
2023-11-16 10:33:14 +0300

    made some changes relating to values and descriptions and mitigated some errors

--- parser/RC_parse_sdf.py
+++ parser/RC_parse_sdf.py
@@ -1,8 +1,10 @@
 import xml.etree.ElementTree as ET
 import os 
-import FreeCAD 
+import re
+#import FreeCAD 
 
-_dir=os.path.join(FreeCAD.getUserAppDataDir(),"Mod","RobotCreator","Resources","formats","sdf")
+#_dir=os.path.join(FreeCAD.getUserAppDataDir(),"Mod","RobotCreator","Resources","formats","sdf")
+_dir='/home/mbayi/Documents/RobotCreator/Resources/formats/sdf'
 class Element_Attribute:
     pass
 
@@ -10,29 +12,40 @@ class sdf_parse:
     def __init__(self,version='1.7',file='root.sdf'):
         #initialize directory with the root.sdf
         self.root_dir=os.path.join(_dir,version,file)
+        self.version=version
         #create a dictionary 
         self.Main_ElemDict={}
         #parse tree and store the result in local variable tree
         self.tree=self.parse_tree(self.root_dir)
         #get the root element 
         self.root=self.tree.getroot()
-        #populate the dictionary with data 
-       
-        # call the tree with the parent  root element
-        self.Main_ElemDict=self.populate_structure(self.root)
         
         #a stack of parent elements
         #this stack is provided to allow for  having a parent  key in the dictionary 
         #to help when creating an xnl tree by adding subnode 
         self.parent_stack=[]
         
+        #populate the dictionary with data 
+        # call the tree with the parent  root element
+        self.Main_ElemDict=self.populate_structure(self.root)
+        
+        
     def populate_structure(self,Element):
         #add elements to structure 
-        ElemDict={};
+        ElemDict={}
         # append alement to  list 
         self.parent_stack.append(Element)
         # set tag name 
-        ElemDict["tag"]=Element.attrib["name"]
+        
+        #skip child if name property is not available
+        #considering the copydata element in plugin.sdf 
+        #that should not be included 
+        try:
+            ElemDict["tag"]=Element.attrib["name"]
+        except:
+            self.parent_stack.pop()         
+            return ElemDict  
+        
         #find all attributes and store them  in a list
         #this is due to some classes having multiple attributes 
         #store attibute dictionary and descritpion in a tuple 
@@ -41,21 +54,27 @@ class sdf_parse:
         #list to store class atrributes 
         self._attr=[]
         for result in Element.findall("attribute"):
-            
-            description=result.find("description").text
+            #remove whitespaces ,tabs and newline characters
+            description=re.sub('\s+',' ',str(result.find("description").text).strip())
             self._attr.append((result.attrib,description))
         #check to see that attributes are not empty
         if len(self._attr)==0:
             ElemDict["attributes"]=None
         else:
             ElemDict["attributes"]=self._attr
-        
-        ElemDict["value"]=Element.text
+        #dont add empty strings 
+        value_str=re.sub('\s+',' ',Element.text).strip()
+        if value_str =='':
+            ElemDict["value"]=None
+        else:
+            ElemDict["value"]=value_str
         
         #set parent node
         if Element ==self.root:
+            #root element will have a none parent 
             ElemDict["parent"]=None
         else:
+            # the parent is most  probably be the previously added element 
             ElemDict["parent"]=self.parent_stack[-2]
               
         
@@ -64,38 +83,52 @@ class sdf_parse:
         # None is used if the following keys are not part of the attributes 
         
         #check fo type 
-        if "type" in Element.attrib.keys:
+        if "type" in dict(Element.attrib):
             ElemDict["type"]=Element.attrib["type"]
         else:
              ElemDict["type"]=None
 
         #check for default
-        if "default" in Element.attrib.keys:
+        if "default" in dict(Element.attrib):
             ElemDict["default"]=Element.attrib["default"]
         else:
              ElemDict["default"]=None
         
         #check for the required key 
-        if "required" in Element.attrib.keys:
+        if "required" in dict(Element.attrib):
             ElemDict["required"]=Element.attrib["required"]
         else:
              ElemDict["required"]=None
+        
+        #create a children key
+        ElemDict["children"]=[]
         #now   loop  through every other children and store their data in the 
         #data in the children field 
         for child in Element:
             if child.tag =="include":
                 #if model is 
                 file=child.attrib["filename"]
-                self.c_dir= self.root_dir=os.path.join(_dir,self.version,file)
+                self.c_dir=os.path.join(_dir,self.version,file)
             
                 #child tree aka c_tree
                 self.c_tree=self.parse_tree(self.c_dir)
                 self._c_root=self.c_tree.getroot()
-                ElemDict["children"].append(self.populate_structure(self._c_root))
-            elif child.tag !=" description" and child.tag != "include":
-                 ElemDict["children"].append(self.populate_structure(child))
+                _c=self.populate_structure(self._c_root)
+                #dont add empty dictionaries
+                #some elements will be skipped
+                if len(_c) !=0:
+                    ElemDict["children"].append(_c)
+                    
+            elif child.tag =="element":
+                _c=self.populate_structure(child)
+                if len(_c) !=0:
+                    ElemDict["children"].append(_c)
+            #add description item 
+            elif child.tag =="description":
+                ElemDict["description"]=re.sub('\s+',' ',str(child.text).strip())
             else:
-                print("just found a new tag\n :",child.tag)
+               pass 
+           
         #remove last  element form list 
         self.parent_stack.pop()         
         return ElemDict   
@@ -109,3 +142,7 @@ class sdf_parse:
     @property
     def Tree(self):
         return self.Main_ElemDict
+if __name__=="__main__":  
+    s=sdf_parse()
+    d=s.Tree
+    print("done with this stage hopefully")
